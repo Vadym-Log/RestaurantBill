9d0375f13728c7cd3e4a251ac5996eb8022f834f
Vadym-Log <94236146+Vadym-Log@users.noreply.github.com>
2021-11-13 21:54:41 +0200

    Add files via upload

---
+++ Restaurant_Bill_Calculator/TempProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Bill_Calculator
+{
+    public class TempProduct
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
